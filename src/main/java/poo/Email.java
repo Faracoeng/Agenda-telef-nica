cb3b9bd7129b5f2e4e830ce74b53b929532a59e7
Faracoeng <alfmazucheli>
2019-04-16 14:56:35 -0300

    teste email pronto

--- src/main/java/poo/Email.java
+++ src/main/java/poo/Email.java
@@ -3,29 +3,41 @@ package poo;
 import java.util.HashMap;
 
 public class Email {
-    //Atributos
-    private HashMap<String, String> dados = new HashMap<>();
+    private HashMap <String, String> tabelaDeEmails;
 
-    //Métodos
+    private boolean validacaoEmail(String email){
+        String emailER = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+        return email.matches(emailER);
+    }
 
-    public Email(){}
+    public Email(){
+        tabelaDeEmails = new HashMap<>();
+    }
 
-    public boolean add(String r, String e){
+    public boolean add(String rotulo, String email){
+        if(!this.validacaoEmail(email)) return false;
+        if(tabelaDeEmails.containsKey(rotulo)) return false;
+        tabelaDeEmails.put(rotulo, email);
         return true;
     }
 
-    public boolean remove(String r){
+    public boolean remove(String rotulo){
+        if(!tabelaDeEmails.containsKey(rotulo)) return false;
+        tabelaDeEmails.remove(rotulo);
         return true;
     }
 
-    public boolean update(String r, String e){
+    public boolean update(String rotulo, String email){
+        if(!this.validacaoEmail(email)) return false;
+        if(!this.tabelaDeEmails.containsKey(rotulo)) return false;
+        this.tabelaDeEmails.put(rotulo, email);
         return true;
     }
 
     @Override
     public String toString() {
         return "Email{" +
-                "dados=" + dados +
+                "tabelaDeEmails=" + tabelaDeEmails +
                 '}';
     }
 }
--- src/test/java/TestaEmail.java
+++ src/test/java/TestaEmail.java
@@ -2,15 +2,34 @@ package poo;
 
 import org.junit.Assert;
 import org.junit.Test;
-//
-//public class TestaEmail {
-//
-//    public void testaEmail(){
-//        @Test
-//        Email testarEmail = new Email();
-//        //Assert.assertTrue();
-//        //Assert.assertFalse(); Verificr assim
-//
-//    }
 
-//}
+public class TestaEmail {
+
+    @Test
+    public void add(){
+
+        Email testarEmail = new Email();
+        Assert.assertFalse("Email inválido", testarEmail.add("pessoal", "erdina@gmail"));
+        Assert.assertTrue("Email válido", testarEmail.add("pessoal", "ANDRÉ@gmail.com"));
+        Assert.assertFalse("Rótulo duplicado", testarEmail.add("pessoal", "andre@gmail.com"));
+        Assert.assertFalse("Email inválido", testarEmail.add("pessoal", "@gmail.com"));
+        Assert.assertFalse("Email inválido", testarEmail.add("pessoal", "andregmail.com"));
+    }
+
+    @Test
+    public void remove(){
+        Email e = new Email();
+        e.add("pessoal", "camilla@gmail.com");
+        Assert.assertFalse("Rótulo inexistente", e.remove("trabalho"));
+        Assert.assertTrue("Remoção válida", e.remove("pessoal"));
+    }
+
+    @Test
+    public void update(){
+        Email e = new Email();
+        e.add("pessoal", "camilla@gmail.com");
+        Assert.assertFalse("Rótulo inexistente", e.update("trabalho", "novo@gmail.com"));
+        Assert.assertTrue("Update válido", e.update("pessoal", "novo@gmail.com"));
+    }
+
+}
