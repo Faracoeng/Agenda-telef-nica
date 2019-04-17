7325a2fd9415fa810bd1a4a3b0ce0692223635f4
Faracoeng <alfmazucheli>
2019-04-17 10:45:31 -0300

    classe Telefone pronta

--- src/main/java/poo/Telefone.java
+++ src/main/java/poo/Telefone.java
@@ -11,9 +11,12 @@ public class Telefone {
     public Telefone(){
         tabelaDeTelefones = new HashMap<>();
     }
+    private boolean validacaoTelefone(String numero){
+        String telefoneER = "^[1-9]{2}([9][1-9][0-9]{7}|[1-9][0-9]{7})$";
+        return numero.matches(telefoneER);
+    }
 
     public boolean add(String r, String n){
-
 
         if(!n.matches(tel)) return false;
         if(tabelaDeTelefones.containsKey(r)) return false;
@@ -22,13 +25,15 @@ public class Telefone {
     }
 
     public boolean remover(String r){
-
-
+        if(!tabelaDeTelefones.containsKey(r)) return false;
+        tabelaDeTelefones.remove(r);
         return true;
     }
 
     public boolean update(String r, String n){
-
+        if(!this.validacaoTelefone(n)) return false;
+        if(!this.tabelaDeTelefones.containsKey(r)) return false;
+        tabelaDeTelefones.put(r, n);
         return true;
     }
 
