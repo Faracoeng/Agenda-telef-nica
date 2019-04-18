950fec235c7c43bb1ffca4c400c43da4354d90c4
André Luiz <alfmazucheli@gmail.com>
2019-04-18 16:01:14 -0300

    E-mail comentada

--- src/main/java/poo/Email.java
+++ src/main/java/poo/Email.java
@@ -10,27 +10,28 @@ public class Email {
     //Métodos
     private boolean validacaoEmail(String email){
 
-        return email.matches(emailER);      //
+        return email.matches(emailER);      //  Verifica se email é valido
     }
 
     public Email(){
-        tabelaDeEmails = new HashMap<>();
+        tabelaDeEmails = new HashMap<>();   //  Construtor define Email como uma HashMap
     }
 
-    public boolean add(String r, String e){
-        if(!this.validacaoEmail(e)) return false;
-        if(tabelaDeEmails.containsKey(r)) return false;
-        tabelaDeEmails.put(r, e);
+    public boolean add(String r, String e){ // Esse método adiciona um e-mail
+
+        if(!this.validacaoEmail(e)) return false;   //se e-mail nao for válido
+        if(tabelaDeEmails.containsKey(r)) return false; //  Se conter a chave "r"
+        tabelaDeEmails.put(r, e); // então e-mail é adicionado
         return true;
     }
 
-    public boolean remove(String r){
-        if(!tabelaDeEmails.containsKey(r)) return false;
-        tabelaDeEmails.remove(r);
+    public boolean remove(String r){        // Remove um e-mail especifico da HashMap
+        if(!tabelaDeEmails.containsKey(r)) return false;    //Se e-mail existir na HashMap
+        tabelaDeEmails.remove(r);   //remove ele
         return true;
     }
 
-    public boolean update(String r, String e){
+    public boolean update(String r, String e){          //Igual anteriores
         if(!this.validacaoEmail(e)) return false;
         if(!this.tabelaDeEmails.containsKey(r)) return false;
         this.tabelaDeEmails.put(r, e);
