7e43789d5b89b6e5fb8851c6f87200a8fc735968
Faracoeng <alfmazucheli@gmail.com>
2019-04-19 20:07:44 -0300

    VerificaPosicao() implementado

--- src/main/java/poo/Agenda.java
+++ src/main/java/poo/Agenda.java
@@ -18,7 +18,8 @@ public class Agenda {
 
     public boolean removePessoa(String n, String s){
         for (Pessoa p : contatos) { //PERCORRE A LISTA USANDO [FOR EACH]
-            if(p.toString() == n + s){                  // COMPARA RETORNO DO METODO toString(), qué uma concatenaçao do nome e sobrenome, com parametos concatenados
+            if(p.toString() == n + s){                  // COMPARA RETORNO DO METODO toString(), qué uma concatenaçao
+                                                        // do nome e sobrenome, com parametos concatenados
                 contatos.remove(contatos.indexOf(p));   // Se forem iguais, remove pessoa associada ao indice atual
                 return true;
             }
@@ -31,6 +32,12 @@ public class Agenda {
         if(pIndex < contatos.size()) return false;  // kimita indice ser dentro do tamanho da lista(tratamento d erro)
         // As verificações acima serão necessários em vário métodos, é uma boa prática criar um método para fazer o processo
         return contatos.get(pIndex).addTelefone(r, n); //adiciona rótulo e nove na posição desejada
+    }
+
+    private boolean verificaPosicao(int pos){       //esse método é utilizado pela classe Agenda() para
+                                                    // validar entrada de parametros de vários métodos
+        if(contatos.size() == 0) return false;
+        return pos<contatos.size();
     }
 
 
